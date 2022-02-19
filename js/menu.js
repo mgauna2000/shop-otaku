23abae4b131a341859b77cd4f4ebcd4ff688f8a4
Martin <martingauna2000@gmail.com>
2022-02-19 19:41:56 -0300

    styles menu hover

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 let btnMenu = document.getElementById("btn-menu");
-let menu = document.getElementById("menu");
+let mainNav = document.getElementById("main-nav");
 
 btnMenu.addEventListener('click', function() {
-    menu.classList.toggle('mostrar');
+    mainNav.classList.toggle('mostrar');
 });
